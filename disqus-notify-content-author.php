be14bbf2eabc0c02e679f9e14b23755e13c7c15d
Janne Cederberg <janne.cederberg@gmail.com>
2014-12-23 05:18:56 +0200

    RC1 for v1.1 adding spam status checking relating to pingbacks

--- disqus-notify-content-author.php
+++ disqus-notify-content-author.php
@@ -3,7 +3,7 @@
  * Plugin Name: Disqus Notify Post/Page Author
  * Plugin URI: http://wordpress.org/plugins/disqus-notify-content-author/
  * Description: When using Disqus Comment System, notify post/page author of comments by email without hacking the Disqus plugin.
- * Version: 1.0.4
+ * Version: 1.1
  * Author: Janne Cederberg
  * Author URI: http://opetus.tv
  * License: GPLv2
@@ -11,8 +11,33 @@
 
 defined('ABSPATH') or die("See no evil, hear no evil, speak no evil");
 
-function disqus_notify_content_author($comment_id, $comment = null) {
-    wp_notify_postauthor($comment_id);
+/**
+ * Action for notifying author of comments/pingbacks
+ *
+ * The main action hook for notifying authors when new comments
+ * or pingbacks are added to posts and pages.
+ *
+ * @author jcederberg
+ */
+function disqus_notify_content_author($comment_id, $comment_obj = null) {
+	if ( _disqus_notify_content_author__is_not_spam($comment_obj) ) {
+		wp_notify_postauthor($comment_id);
+	}
 }
 
-add_action('wp_insert_comment', 'disqus_notify_content_author');
+/**
+ * Check if a pingback is marked as spam (by for example Akismet)
+ *
+ * With pingbacks being sent to the site while also using Disqus,
+ * the plugin is seemingly causing spam pingbacks caught by Akismet
+ * to be sent out to the post author.
+ *
+ * @author jcederberg, kraftbj
+ * @see https://wordpress.org/support/topic/check-spam-status
+ */
+function _disqus_notify_content_author__is_not_spam($comment_obj) {
+	$comment_status = $comment_obj->comment_approved;
+	return ( $comment_status != 'spam' );
+}
+
+add_action('wp_insert_comment', 'disqus_notify_content_author', 99, 2);
